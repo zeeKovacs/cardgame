091c569fc629aa0ce2879e6f706bb1d49360cf41
Szűcs Áron <aron.szucs.miskolc@gmail.com>
2019-02-11 13:00:11 +0100

    done

--- src/main/java/com/codecool/XMLHandler.java
+++ src/main/java/com/codecool/XMLHandler.java
@@ -2,7 +2,6 @@ package com.codecool;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -11,52 +10,69 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Set;
 
 public class XMLHandler {
     private String filepath;
     private Document document;
+    private Deck deck;
+    private GameData gameData;
+
 
     public XMLHandler(String filepath) {
         this.filepath = filepath;
     }
 
+    public GameData getGameData() {
+        return gameData;
+    }
 
-    public Deck loadDeck() throws XMLLoadError {
+    public Deck getDeck() {
+        return deck;
+    }
+
+    public void load() throws XMLLoadError {
 
         getDocument();
-        GameData gameData = getGameData();
+        loadGameData();
         Set<String> fields = gameData.getFieldIds();
-        Deck deck = new Deck("Test deck");
 
         NodeList docCardList = document.getElementsByTagName("Card");
+        deck = new Deck("Test Deck");
         for (int i = 0; i < docCardList.getLength(); i++) {
             Element docCard = (Element) docCardList.item(i);
             int id = Integer.parseInt(docCard.getAttribute("id"));
             String name = docCard.getAttribute("name");
-            System.out.println(name);
             Card card = new Card(id, name);
             for (String field : fields) {
-
                 Element docField = (Element) docCard.getElementsByTagName(field).item(0);
-                String fieldName = docField.getTagName();
-                int fieldValue = Integer.parseInt( docField.getAttribute("val"));
+                String fieldName;
+                int fieldValue;
+                try {
+                    fieldName = docField.getTagName();
+                } catch (NullPointerException e) {
+                    throw new XMLLoadError("Invalid or missing field name at card id: " + String.valueOf(id));
+                }
+                try {
+                    fieldValue = Integer.parseInt(docField.getAttribute("val"));
+                } catch (NumberFormatException e) {
+                    throw new XMLLoadError("Invalid field value at card id: " + String.valueOf(id));
+                }
                 card.addStat(fieldName, fieldValue);
             }
-
+            deck.addCard(card);
         }
-
-        return new Deck("dfsd");
     }
 
-    private GameData getGameData() {
+    private GameData loadGameData() {
+        Element docData = (Element) document.getElementsByTagName("Data").item(0);
+        String name = docData.getAttribute("name");
+        String typeSingular = docData.getAttribute("type_singular");
+        String typePlural = docData.getAttribute("type_plural");
+        gameData = new GameData(name, typeSingular, typePlural);
+
         Element root = (Element) document.getElementsByTagName("Fields").item(0);
         NodeList docFieldList = root.getElementsByTagName("Field");
-        GameData gameData = new GameData();
-
 
         for (int i = 0; i < docFieldList.getLength(); i++) {
 
