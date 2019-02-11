99d9141419719831607d81365462f580c1f35f34
Szűcs Áron <aron.szucs.miskolc@gmail.com>
2019-02-11 10:58:15 +0100

    fish

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         UI ui = new UI();
         ui.displayMenu();
+
     }
-    
+
+
+
 }
---
+++ src/main/java/com/codecool/XMLLoadError.java
@@ -0,0 +1,4 @@
+package com.codecool;
+
+public class XMLLoadError extends Error {
+}
