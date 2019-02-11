b7cbbe8a1e79eeb9f9e64328a2d0739065ef0dab
Zoltan <zolee95@gmail.com>
2019-02-11 10:33:35 +0100

    toString in deck

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -3,6 +3,7 @@ package com.codecool;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi!");
+        UI ui = new UI();
+        ui.displayMenu();
     }
 }
